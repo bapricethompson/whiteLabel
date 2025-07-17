899bd6fc8860ddf5fc10ad2d2ca540ae191125a3
brooklyn <baprice01@gmail.com>
2025-07-17 13:18:06 -0600

    tests and more refactoring

--- functions/modules/checkadmin.js
+++ functions/modules/checkadmin.js
@@ -1,11 +1,14 @@
+const { isAdmin } = require("../../src/services/authService");
+
 function checkAdminPermission(req, res, next) {
-  console.log("NEW ADMIN");
   const user = req.user;
-  if (!user || !user.permissions || !user.permissions.includes("Admin")) {
-    return res
-      .status(401)
-      .json({ error: "Admin permission required to access this route." });
+
+  if (!isAdmin(user)) {
+    return res.status(401).json({
+      error: "Admin permission required to access this route.",
+    });
   }
+
   next();
 }
 
